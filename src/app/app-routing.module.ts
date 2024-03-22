7b2fd9742d772773655f2e264c49b23a63fbe3ab
DavideGitRepo <164370303+DavideGitRepo@users.noreply.github.com>
2024-03-22 17:17:14 +0100

    Fix

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,16 +1,16 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-import { LoginComponent } from './components/login/login.component';
-import { HomeComponent } from './components/home/home.component';
-import { RegisterComponent } from './components/register/register.component';
-import { loggedGuard } from './guards/logged.guard';
+ import { LoginComponent } from './components/login/login.component';
+ import { HomeComponent } from './components/home/home.component';
+ import { RegisterComponent } from './components/register/register.component';
+ import { loggedGuard } from './guards/logged.guard';
 
 const routes: Routes = [
-  {path:'', component: HomeComponent, canActivate:[loggedGuard]},
-  {path:'**', component: HomeComponent , canActivate:[loggedGuard]},
-  {path:'login', component: LoginComponent},
-  {path:'signin', component: RegisterComponent},
-
+   {path:'', component: HomeComponent, canActivate:[loggedGuard]},
+   {path:'login', component: LoginComponent},
+   {path:'signin', component: RegisterComponent},
+   {path:'**', component: LoginComponent },
+   
 ];
 
 @NgModule({
